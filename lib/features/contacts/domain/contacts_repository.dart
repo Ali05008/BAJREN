31828eb81940f8d7e3e3643684cc24f210a37794
Ali05008 <alihassan0v0@gmail.com>
2026-08-11 05:02:53 +0300

    Create contacts_repository.dart

---
+++ lib/features/contacts/domain/contacts_repository.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+
+import 'contact.dart';
+
+class ContactLookupResult extends Equatable {
+  final String uid;
+  final String displayName;
+
+  const ContactLookupResult({
+    required this.uid,
+    required this.displayName,
+  });
+
+  @override
+  List<Object?> get props => [uid, displayName];
+}
+
+abstract class ContactsRepository {
+  Stream<List<Contact>> watchContacts(String ownerUid);
+
+  Future<void> ensureOwnPublicProfile({
+    required String uid,
+    required String displayName,
+  });
+
+  Future<ContactLookupResult?> lookupUserByUid(String uid);
+
+  Future<void> addContact({
+    required String ownerUid,
+    required String contactUid,
+    required String contactDisplayName,
+    required String ownerDisplayName,
+  });
+
+  Future<void> removeContact({
+    required String ownerUid,
+    required String contactUid,
+  });
+}
+
+class ContactException implements Exception {
+  final String code;
+  final String message;
+
+  const ContactException(this.code, this.message);
+
+  @override
+  String toString() => 'ContactException($code): $message';
+}
